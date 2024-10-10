0e480deb3eb624138b4f278d9a24a0a8b092d5a9
AIsiteru98 <132365672+AIsiteru98@users.noreply.github.com>
2024-10-10 16:14:58 +0900

    main dfd

---
+++ SuperGap_Part2/SuperGap_Part2/SuperGap_Part2App.swift
@@ -0,0 +1,17 @@
+//
+//  SuperGap_Part2App.swift
+//  SuperGap_Part2
+//
+//  Created by 이재용 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperGap_Part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperGap_Part2/SuperGap_Part2Tests/SuperGap_Part2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SuperGap_Part2Tests.swift
+//  SuperGap_Part2Tests
+//
+//  Created by 이재용 on 10/10/24.
+//
+
+import Testing
+@testable import SuperGap_Part2
+
+struct SuperGap_Part2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
